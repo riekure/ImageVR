27c37ec4f2e4be74834206e23b03d9752ea194fe
riekure <s2darl0uvgia24@gmail.com>
2019-07-29 22:36:18 +0900

    update exit button

---
+++ Assets/ImageVR/Scripts/ImageController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageController : MonoBehaviour
+{
+    // Click
+    public void OnClick()
+    {
+        // アプリケーション終了
+        Application.Quit();
+    }
+}
